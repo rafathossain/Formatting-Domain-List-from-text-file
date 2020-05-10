4e4825498d7abd59f9ee25bdec61d55d4f27117d
Rafat Hossain <rafathossain96@gmail.com>
2020-05-11 02:17:18 +0600

    Minor Bug Fixed

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-file_name = "Domains_list.txt"
+domain_file_name = "Domains_list.txt"
+url_file_name = "URLs_list.txt"
 
-file = open(file_name, "r")
+
+domain_file = open(domain_file_name, "r")
+url_file = open(url_file_name, "r")
 domain_dictionary = []
-key = 0
+url_dictionary = []
 
-
-for line in file:
+for line in domain_file:
 	# print(line.strip())
 	line = line.strip()
 	if line != "":
@@ -20,10 +22,27 @@ for line in file:
 			domain_dictionary.append(final_domain)
 			# print(final_domain)
 
-file.close()
-output_file = open("Domains_list_final.txt", "a")
+for line in url_file:
+	# print(line.strip())
+	line = line.strip()
+	if line != "":
+		full_url = line
+		# full_url = full_url.split("/")[0]
+		final_url = full_url.strip()
+		if final_url[0:4] != "www.":
+			final_url = "www." + final_url
+		url_dictionary.append(final_url)
+
+domain_file.close()
+output_file = open("Domains_And_URL_List.txt", "w")
+output_file.write("#Domains\n")
 
 for domains in domain_dictionary:
 	output_file.write("domain:" + domains + "\n")
+
+output_file.write("\n#URLs\n")
+
+for url in url_dictionary:
+	output_file.write(url + "\n")
 
 output_file.close()
